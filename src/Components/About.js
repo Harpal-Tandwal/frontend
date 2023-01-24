e9f544e15d9b3921dda6f00a051af0d164beebd6
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-24 19:34:14 +0530

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,22 +7,97 @@ const About=() =>{
   const  [email,setEmail]=useState('');
   const  [projectName,setprojectName]=useState('');
   const [prjct,setProjects]= useState("");
-
-
   const navigate = useNavigate();
+
+  
+  var Cookie =
+{
+   set: function(name, value, days)
+   {
+      var domain, domainParts, date, expires, host;
+
+      if (days)
+      {
+         date = new Date();
+         date.setTime(date.getTime()+(days*24*60*60*1000));
+         expires = "; expires="+date.toGMTString();
+      }
+      else
+      {
+         expires = "";
+      }
+
+      host = window.location.host;
+      if (host.split('.').length === 1)
+      {
+         // no "." in a domain - it's localhost or something similar
+         document.cookie = name+"="+value+expires+"; path=/";
+      }
+      else
+      {
+         // Remember the cookie on all subdomains.
+          //
+         // Start with trying to set cookie to the top domain.
+         // (example: if user is on foo.com, try to set
+         //  cookie to domain ".com")
+         //
+         // If the cookie will not be set, it means ".com"
+         // is a top level domain and we need to
+         // set the cookie to ".foo.com"
+         domainParts = host.split('.');
+         domainParts.shift();
+         domain = '.'+domainParts.join('.');
+
+         document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+
+         // check if cookie was successfuly set to the given domain
+         // (otherwise it was a Top-Level Domain)
+         if (Cookie.get(name) == null || Cookie.get(name) != value)
+         {
+            // append "." to current domain
+            domain = '.'+host;
+            document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+         }
+      }
+   },
+
+   get: function(name)
+   {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i=0; i < ca.length; i++)
+      {
+         var c = ca[i];
+         while (c.charAt(0)==' ')
+         {
+            c = c.substring(1,c.length);
+         }
+
+         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+   },
+
+   erase: function(name)
+   {
+      Cookie.set(name, '', -1);
+   }
+};
+
+
 
  
  const callAboutPage = async()=>{
   try{
-    
+    var token= Cookie.get("jwtoken");
+    console.log("token picked from cookies :",token)
     const res= await fetch("https://iot-backend-xvij.onrender.com/aboutme",{
-      method:"GET",
+      method:"POST",
       headers:{
-        // Accept:"application/json",
+        Accept:"application/json",
        "Content-Type":"application/json"
       },
-      mode:"cors",
-      credentials: 'include'
+      body:JSON.stringify({token:token})
     });
     const data = await res.json();
     console.log(data.email);
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,14 +2,84 @@ import React,{useState} from 'react'
 import { NavLink ,useNavigate} from 'react-router-dom'
 import Footer from './footer';
 
-import { useCookies } from 'react-cookie';
+
 
 
 export default function Login() {
-  const [cookies, setCookie] = useCookies(['user']);
-  const handle = () => {
-    setCookie('jwtoken', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NkZjA1NGFmZGYyNmFhMTRlYTQzOTEiLCJpYXQiOjE2NzQ0OTk3Njl9.4TNxrPX5YIhYvf3mOmoF61Xmj0A8GnWbrFgVNWuL-14", { path: '/' });
-  };
+
+  var Cookie =
+{
+   set: function(name, value, days)
+   {
+      var domain, domainParts, date, expires, host;
+
+      if (days)
+      {
+         date = new Date();
+         date.setTime(date.getTime()+(days*24*60*60*1000));
+         expires = "; expires="+date.toGMTString();
+      }
+      else
+      {
+         expires = "";
+      }
+
+      host = window.location.host;
+      if (host.split('.').length === 1)
+      {
+         // no "." in a domain - it's localhost or something similar
+         document.cookie = name+"="+value+expires+"; path=/";
+      }
+      else
+      {
+         // Remember the cookie on all subdomains.
+          //
+         // Start with trying to set cookie to the top domain.
+         // (example: if user is on foo.com, try to set
+         //  cookie to domain ".com")
+         //
+         // If the cookie will not be set, it means ".com"
+         // is a top level domain and we need to
+         // set the cookie to ".foo.com"
+         domainParts = host.split('.');
+         domainParts.shift();
+         domain = '.'+domainParts.join('.');
+
+         document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+
+         // check if cookie was successfuly set to the given domain
+         // (otherwise it was a Top-Level Domain)
+         if (Cookie.get(name) == null || Cookie.get(name) != value)
+         {
+            // append "." to current domain
+            domain = '.'+host;
+            document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+         }
+      }
+   },
+
+   get: function(name)
+   {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i=0; i < ca.length; i++)
+      {
+         var c = ca[i];
+         while (c.charAt(0)==' ')
+         {
+            c = c.substring(1,c.length);
+         }
+
+         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+   },
+
+   erase: function(name)
+   {
+      Cookie.set(name, '', -1);
+   }
+};
 
 
   const  [email,setEmail]=useState('')
@@ -19,9 +89,9 @@ export default function Login() {
   const LoginUser=async (e)=>{
     e.preventDefault();
     // console.log("A")
-  
 
-    const res= await fetch("https://iot-backend-xvij.onrender.com/signin",{
+
+    const res= await fetch("https://iot-backend-xvij.onrender.com/signin",{ //https://iot-backend-xvij.onrender.com
       method:"POST",
       headers:{
         "Content-Type":"application/json"
@@ -32,14 +102,16 @@ export default function Login() {
       )
 
     })
-    const data = res.json();
-
+   
+    const data =  await res.json();
+ 
+console.log( " data get from login request",data.token)
     if(res.status===400 || !data){
       window.alert("cant login")
     }else{
       window.alert("login succesful");
       navigate("/about")
-      handle();
+      Cookie.set('jwtoken', data.token ,"5");
     }
   }
   return (
